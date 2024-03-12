89b3d779df53226c291708664b6e7299c1d4df0a
iiacoban42 <i.p.iacoban@gmail.com>
2024-03-12 01:46:56 +0100

    Add script for starting ganache

---
+++ contracts/start_ganache.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ganache-cli -m "pistol kiwi shrug future ozone ostrich match remove crucial oblige cream critic" --account_keys_path keys.json --accounts 55 --block-time 5 --gasPrice 100000000000
